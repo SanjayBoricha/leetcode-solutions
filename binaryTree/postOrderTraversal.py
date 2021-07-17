6c286a5fdd20461525b288dfdf35bf26fe996bb1
SanjayBoricha <sanjayboricha1001@gmail.com>
2021-07-17 17:43:43 +0530

    Binary tree traversal

---
+++ binaryTree/postOrderTraversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> list:
+        if not root:
+            return root
+
+        array = []
+
+        self.travel(root, array)
+
+        return array
+
+    def travel(self, node: TreeNode, array: list):
+        if node.left:
+            self.travel(node.left, array)
+
+        if node.right:
+            self.travel(node.right, array)
+
+        array.append(node.val)
+
+
+tree = TreeNode(1, None, TreeNode(2, TreeNode(3)))
+
+print(Solution().inorderTraversal(tree))
